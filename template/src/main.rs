0a9c9afdac60c4b0801b199c3c5fbb579edf1bc3
Kien Tuong Truong <info@ktruong.dev>
2021-12-05 12:59:21 +0100

    feat: create template

---
+++ template/src/main.rs
@@ -0,0 +1,20 @@
+use itertools::Itertools;
+use Regex::re;
+
+fn part1() -> u32 {
+    include_str!("./input.txt")
+        .lines()
+}
+
+fn part2() -> u32 {
+    include_str!("./input.txt")
+        .lines()
+}
+
+fn main() {
+    println!(
+        "{}, {}",
+        part1(),
+        part2()
+    )
+}
